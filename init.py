e0da6e5b7e4863d97c81a5e5d0f660a759dc72cc
mblue8nix <mblue8nix@gmail.com>
2022-12-31 16:10:22 -0800

    Create init.py
    added init.py for all my scripts that will be added.

---
+++ init.py
@@ -0,0 +1,31 @@
+from mysqlsh.plugin_manager import plugin, plugin_function
+
+@plugin
+class blue:
+    """
+    Group of plugins I created.
+
+EXAMPLE Usge:
+
+    blue.getOriented()          #Runs multiple sql statements to get oriented.
+    blue.getMtsBalance()        #Runs MTS Balance Metrics.  Creates view if it does not exist.
+    blue.getEmptyPartitions()   #Look at empty Partitions and can pass a table name.
+    blue.statement_error        #Shows statements that errored out.
+
+#Adds to report function
+
+    \show dbsize          #Shows Schema Sizes
+        \show dbisze -l   #Limit rows
+
+    \show tblsize         #Shows table Sizes
+                     -l   #Limit rows
+    """
+
+from blue import dbsize
+from blue import tblsize
+from blue import orient_db
+from blue import mts_balance
+from blue import empty_partitions
+from blue import statement_error
+from blue import statement_analy
+from blue import table_stat_sum
